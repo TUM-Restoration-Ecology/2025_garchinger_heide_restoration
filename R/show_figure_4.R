31d2737c40f936e8e4015ae4100c67c89708805e
markus1bauer <markus1.bauer@tum.de>
2025-08-17 18:27:28 -0400

    Update figures

--- R/show_figure_4.R
+++ R/show_figure_4.R
@@ -1,10 +1,10 @@
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Garchinger Heide
-# NMDS ####
+# Management Garchinger Heide restoration sites
+# Functional plant traits ####
 # Show figure 4
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# Sina Appeltauer, Markus Bauer
-# 2025-03-03
+# Markus Bauer
+# 2025-08-17
 
 
 
@@ -17,207 +17,26 @@
 ### Packages ###
 library(here)
 library(tidyverse)
-library(vegan)
+library(patchwork)
 
 ### Start ###
-rm(list = setdiff(ls(), c("graph_a", "graph_b", "graph_c", "graph_d")))
-
-### Functions ###
-theme_mb <- function() {
-  theme(
-    panel.background = element_rect(fill = "white"),
-    text = element_text(size = 9, color = "black"),
-    strip.text = element_text(size = 10),
-    axis.text = element_text(angle = 0, hjust = 0.5, size = 9,
-                             color = "black"),
-    axis.title = element_text(angle = 0, hjust = 0.5, size = 9,
-                              color = "black"),
-    axis.line = element_line(),
-    legend.key = element_rect(fill = "white"),
-    legend.position = "right",
-    legend.margin = margin(0, 0, 0, 0, "cm"),
-    legend.text = element_text(size = 10),
-    plot.margin = margin(0, 0, 0, 0, "cm")
-  )
-}
-
-vegan_cov_ellipse <- function(cov, center = c(0, 0), scale = 1, npoints = 100) {
-  theta <- (0:npoints) * 2 * pi / npoints
-  circle <- cbind(cos(theta), sin(theta))
-  t(center + scale * t(circle %*% chol(cov)))
-}
-
-#### * Load data sites ####
-
-sites <- read_csv(
-  here("data", "processed", "data_processed_sites.csv"),
-  col_names = TRUE, na = c("na", "NA", ""), col_types =
-    cols(
-      .default = "?"
-    )) %>% 
-  arrange(id)
-
-species <- read_csv(
-  here("data", "processed", "data_processed_species.csv"),
-  col_names = TRUE, na = c("na", "NA", ""), col_types = cols(.default = "?")
-) %>%
-  pivot_longer(-accepted_name, names_to = "id", values_to = "value") %>%
-  semi_join(sites, by = "id") %>%
-  arrange(id) %>%
-  pivot_wider(names_from = "accepted_name", values_from = "value") %>%
-  column_to_rownames(var = "id")
-
-
-#### * Choosen model ####
-
-base::load(here("outputs", "models", "model_nmds.Rdata"))
-ordi
-
-base::load(here("outputs", "models", "model_nmds_envfit_vector1.Rdata"))
-envfit <- ef_vector1
-
-rm(
-  list = setdiff(
-    ls(), c("sites", "envfit", "ordi", "theme_mb", "vegan_cov_ellipse")
-  )
-)
+rm(list = setdiff(ls(), c("graph_a", "graph_b", "graph_c")))
 
 
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# B Plot ######################################################################
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Plot #######################################################################
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
-#### * Preparation ####
+graph_a / graph_b / graph_c +
+  plot_layout(guides = "keep") +
+  plot_annotation(tag_levels = "A", tag_prefix = "", tag_suffix = "") &
+  theme(plot.tag = element_text(size = 10, face = "bold"))
 
-data_envfit <- envfit %>%
-  scores(display = "vectors") %>%
-  as_tibble(rownames = NA) %>%
-  rownames_to_column(var = "variable") %>%
-  mutate(
-    variable = as_factor(variable),
-    variable = fct_recode(
-      variable,
-      "Vegetation\nheight" = "height_vegetation",
-      "Vegetation\ncover" = "cover_vegetation",
-      "Grass\ncover" = "grass_cover",
-      "Graminoid\ncover" = "graminoid_cover"
-    )
-  )
-
-data_nmds <-  sites %>%
-  select(
-    id, treatment, esy, mowing_date_start, year_hay_transfer,
-    year_topsoil_removal
-    ) %>% # modify group
-  mutate(
-    group = as_factor(treatment), # modify group
-    NMDS1 = ordi$points[, 1],
-    NMDS2 = ordi$points[, 2]
-  ) %>%
-  group_by(group) %>%
-  mutate(mean1 = mean(NMDS1), mean2 = mean(NMDS2))
-
-
-#### * Site scores ####
-
-(graph_a <- ggplot() +
-   geom_point(
-     aes(y = NMDS2, x = NMDS1, shape = esy, color = group, group = group),
-     data = data_nmds,
-     cex = 2, alpha = .8
-   ) +
-   geom_point(
-     aes(y = NMDS2, x = NMDS1),
-     data = data_nmds %>%
-       filter(
-         year_hay_transfer == "1993" & treatment == "cut_summer" |
-         year_topsoil_removal == "1996/2003"
-       ),
-     color = "red",
-     size = 2,
-     shape = 4
-     ) +
-   annotate(
-     "text", x = 1.2, y = 1.4, cex = 3, label = "2D stress = .18"
-     ) +
-   
-   #### * Ellipses ####
-
- stat_ellipse(
-   aes(y = NMDS2, x = NMDS1, color = group, group = group),
-   data = data_nmds,
-   geom = "path", level = 0.95, show.legend = FALSE
- ) +
-   ggrepel::geom_label_repel(
-     aes(x = NMDS1, y = NMDS2, label = variable),
-     data = data_envfit,
-     fill = alpha("white", .6),
-     size = 3,
-     nudge_y = -.17,
-     nudge_x = .1,
-     min.segment.length = Inf
-   ) +
-
-   #### * Envfit variables ####
-
- geom_segment(
-   data = data_envfit,
-   aes(x = 0, xend = NMDS1, y = 0, yend = NMDS2),
-   arrow = arrow(length = unit(0.25, "cm"), type = "closed"),
-   color = "black",
-   linewidth = 1, alpha = 1
- ) +
-   coord_fixed() +
-
-   #### * Design ####
-
- scale_color_manual(
-   labels = c(
-     "control_2003" = "Reference\n2003",
-     "control_2018" = "Reference\n2018",
-     "control_2021" = "Reference\n2021",
-     "cut_summer" = "Mowing\nsummer",
-     "cut_autumn" = "Mowing\nautumn",
-     "topsoil_removal" = "Topsoil\nremoval"
-   ),
-   values = c(
-     "control_2003" = "#a153a6", 
-     "control_2018" = "#de47f5", 
-     "control_2021" = "#f947d1", 
-     "cut_summer" = "#61a161", 
-     "cut_autumn" = "#87ceeb", 
-     "topsoil_removal" = "#b06e13"
-   )
- ) +
-   scale_shape_manual(
-     labels = c(
-       "R22" = "R22:\nHay meadow",
-       "R1A" = "R1A:\nSemi-dry grassland",
-       "R" = "R:\nUnspecified grassland"
-     ),
-     values = c(
-       "R22" = 1,
-       "R1A" = 16,
-       "R" = 3
-     )
-   ) +
-   scale_linetype_manual(values = c(1, 1, 1, 1, 1, 1, 1)) +
-   labs(x = "NMDS1", y = "NMDS2", shape = "", color = "") +
-   theme_mb() +
-   theme(legend.key.height = unit(.9, "cm")))
-
-
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# C Save ######################################################################
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
+### Save ###
 ggsave(
-  here("outputs", "figures", "figure_4_nmds_subset_800dpi_16.5x11cm.tiff"),
-  dpi = 800, width = 16.5, height = 11, units = "cm"
-)
+  here("outputs", "figures", "figure_4_800dpi_9x20cm.tiff"),
+  dpi = 800, width = 9, height = 20, units = "cm"
+  )
---
+++ R/show_figure_4c_seed_mass.R
@@ -0,0 +1,112 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Management Garchinger Heide restoration sites
+# Seed mass ####
+# Show figure 4c
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Markus Bauer
+# 2025-08-17
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Preparation ###############################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+### Packages ###
+library(here)
+library(tidyverse)
+library(ggeffects)
+library(ggbeeswarm)
+
+### Start ###
+rm(list = setdiff(ls(), c("graph_a", "graph_b", "graph_c", "graph_d")))
+
+### Functions ###
+theme_mb <- function() {
+  theme(
+    panel.background = element_rect(fill = "white"),
+    text = element_text(size = 9, color = "black"),
+    strip.text = element_text(size = 10),
+    axis.text = element_text(angle = 0, hjust = 0.5, size = 8.5,
+                             color = "black"),
+    axis.title = element_text(angle = 0, hjust = 0.5, size = 8.5,
+                              color = "black"),
+    axis.line = element_line(),
+    legend.key = element_rect(fill = "white"),
+    legend.position = "none",
+    legend.margin = margin(0, 0, 0, 0, "cm"),
+    plot.margin = margin(0, 0, 0, 0, "cm")
+  )
+}
+
+### Load data ###
+sites <- read_csv(
+  here("data", "processed", "data_processed_sites.csv"),
+  col_names = TRUE, na = c("", "na", "NA"), col_types = cols(
+    .default = "?",
+    treatment = "f"
+  )
+) %>%
+  rename(y = CWM_Seed) %>%
+  mutate(
+    treatment = fct_relevel(
+      treatment, "control_2003", "control_2018", "control_2021", "cut_summer",
+      "cut_autumn", "topsoil_removal"
+    ),
+    treatment = fct_recode(
+      treatment, "Ref.\n2003" = "control_2003",
+      "Ref.\n2018" = "control_2018", "Ref.\n2021" = "control_2021",
+      "Mowing\nsummer" = "cut_summer", "Mowing\nautumn" = "cut_autumn",
+      "Topsoil\nremoval" = "topsoil_removal"
+    )
+  ) #%>%
+  # filter(
+  #   !(id %in% c(
+  #     "X2021tum03", "X2021tum27", "X2021tum43", "X2021tum48", "X2021tum51",
+  #     "X2003roederS11", "X2018roederS12"
+  #   )) # Plots with >=10% of Polygonatum odoratum (seed mass = 0.08 g)
+  # )
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Plot ######################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+data <- sites %>%
+  mutate(y = y * 1000) %>%
+  rename(predicted = y, x = treatment)
+
+(graph_c <- ggplot(
+  data = data,
+  aes(x = x, predicted, color = x, fill = x)
+) +
+    geom_quasirandom(color = "grey20", dodge.width = .6, size = 1, shape = 16) +
+    geom_boxplot(alpha = .5, color = "black") +
+    annotate(
+      "text", x = 1.5, y = 25, size = 3,
+      label = expression(4^th~corner*": n.s.")
+    ) +
+    scale_y_continuous(breaks = seq(0, 100, 5)) +
+    scale_fill_manual(
+      values = c(
+        "Ref.\n2003" = "#f947d1", 
+        "Ref.\n2018" = "#f947d1", 
+        "Ref.\n2021" = "#f947d1", 
+        "Mowing\nsummer" = "#61a161", 
+        "Mowing\nautumn" = "#87ceeb", 
+        "Topsoil\nremoval" = "#b06e13"
+      )
+    ) +
+    labs(x = "", y = expression(CWM ~ seed ~ mass ~ "[" * mg * "]")) +
+    theme_mb())
+
+### Save ###
+ggsave(
+  here("outputs", "figures", "figure_4c_seed_mass_800dpi_9x8cm.tiff"),
+  dpi = 800, width = 9, height = 8, units = "cm"
+  )
